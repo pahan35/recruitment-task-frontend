a232e2539e0db327b0501afdca6cb6e1ef262b0d
Pavlo Zhukov <pahazhukov@gmail.com>
2022-07-07 11:20:55 +0200

    feat: allow change vite server running port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const config = {
+  plugins: [react()],
+};
+
+const { VITE_PORT } = process.env;
+if (VITE_PORT) {
+  const port = parseInt(VITE_PORT, 10);
+  if (Number.isInteger(port)) {
+    Object.assign(config, { server: { port } });
+  }
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(config);
